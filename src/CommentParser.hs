86da033caa4c25ca9a904574c897c085aaaa1bc3
iostreamer-X <iostreamer007@gmail.com>
2017-05-25 19:51:38 +0530

    Refactored a bit
    CommentParser is now more concise at the cost of returning just one error,
    over a list of errors. Achived this using inbuilt functions over my
    own ugly implementations

--- src/CommentParser.hs
+++ src/CommentParser.hs
@@ -1,7 +1,5 @@
 module CommentParser where
 
-import Data.Either
-
 import Text.Parsec.Error (ParseError,newErrorMessage,Message(Expect))
 import Text.Parsec.Pos (initialPos)
 
@@ -9,16 +7,10 @@ import CommentLexer
 import Parser
 import Syntax
 
-parseComment :: String -> Either [ParseError] [Expression]
-parseComment comment = validate $ if all isRight blocks then Right $ getExpressions blocks else Left $ getErrors $ filter isLeft blocks
+parseComment :: String -> Either ParseError [Expression]
+parseComment comment = validate $ concat <$> sequence blocks
   where
     blocks = map parse . metzBlocks $ comment
     validate ex@(Right expressions) = if any isExpression expressions then ex else noExpressionError comment
-    validate errors = errors
-    noExpressionError comm = Left [newErrorMessage (Expect "an expression") (initialPos comm)]
-
-getExpressions :: [Either t [Expression]] -> [Expression]
-getExpressions = concatMap (\(Right expr) -> expr) 
-
-getErrors :: [Either ParseError t] -> [ParseError]
-getErrors = map (\(Left err) -> err)
+    validate err = err
+    noExpressionError comm = Left $ newErrorMessage (Expect "an expression") (initialPos comm)
