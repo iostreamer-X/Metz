ff622428178c7fb20fab0f03170c60c2611b38d2
iostreamer-X <iostreamer007@gmail.com>
2017-05-31 15:54:17 +0530

    Added function to partition blocks
    The function turns a `Block t` into a `(Block Comment, Block Code)

--- src/FileParser.hs
+++ src/FileParser.hs
@@ -1,12 +1,12 @@
 module FileParser where
 
-import Data.List (isPrefixOf)
+import Data.List (isPrefixOf,partition)
 import Data.List.Split
 
 import FileSyntax
 
 isComment :: String -> String -> Bool
-isComment comment commentStart = commentStart `isPrefixOf` comment
+isComment commentStart comment = commentStart `isPrefixOf` comment
 
 splitToBlocks :: String -> [String]
 splitToBlocks = splitOn "\n\n"
@@ -17,3 +17,10 @@ blockParser rawLines = map Block [map Line $ filter (not.null) rl |rl <- rawLine
 parseFile :: FilePath ->  IO [Block a]
 parseFile path = blockParser . map lines . splitToBlocks <$> readFile path
 
+partitionBlocks :: String -> [Block t] -> [(Block Comment, Block Code)]
+partitionBlocks commentStart = map part
+  where
+    part = uncurry mapLines . partition(\(Line l)-> isComment commentStart l) . getBlock
+    mapLines l1 l2 = (Block $ map makeComment l1, Block $ map makeCode l2)
+    makeComment (Line comm) = Line comm :: Line Comment
+    makeCode (Line code) = Line code :: Line Code
